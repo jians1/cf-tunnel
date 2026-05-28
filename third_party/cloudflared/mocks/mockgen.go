9e4bba6ec0e205abe28808a645ff5d3d675d5852
jians1 <gob5673@gmail.com>
2026-05-28 09:44:51 +0800

    chore: trim non-runtime cloudflared artifacts

--- third_party/cloudflared/mocks/mockgen.go
+++
@@ -1,7 +0,0 @@
-//go:build gomock || generate
-
-package mocks
-
-//go:generate sh -c "go run go.uber.org/mock/mockgen -typed -build_flags=\"-tags=gomock\" -package mocks -destination mock_limiter.go -source=../flow/limiter.go Limiter"
-
-//go:generate sh -c "go run go.uber.org/mock/mockgen -typed -build_flags=\"-tags=gomock\" -package mocks -destination mock_resolvers.go -source=../prechecks/resolvers.go"
